c514c3cf90b6effdf49a8156ca8fef02c51a96ee
Ángel Borrás <angeloki1963@gmail.com>
2024-02-22 08:21:24 +0100

    Preguntas sobre que y como realizarlo

---
+++ LletresSecretas.py
@@ -0,0 +1,25 @@
+"""
+1. ¿Que se ha de hacer? Un juego de adivina la palabra
+
+    1.1. La palabra es de 5 letras.
+    
+    1.2. Las letras tienes tres estados: Desconocida "-", 
+    en la posicion correcta "O", en la posicion incorrecta "X"
+         
+    1.3. No se puede perder, el juego continua hasta ganar,
+    entonces se muestra un mensaje de felicitaciones
+
+2. ¿Como lo hacemos?
+
+    2.1. Se precisa una funcion para generar letras aleatorias,
+    sin importar random.
+    
+    2.2. Se precisa una funcion principal que gestiones intentos
+    y proporcione las pistas
+    
+    2.3. Se precisa usar metodos basados en parametros de entrada
+    y salida de variables.
+    
+    2.4. No entiendo
+"""
+
